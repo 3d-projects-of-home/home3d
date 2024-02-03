25ecd89e48657f98ce97cfa029e0e6081a0112af
3d-projects-of-home <158440774+3d-projects-of-home@users.noreply.github.com>
2024-02-03 17:19:39 +0530

    Delete home3d directory

--- home3d/service/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from service.models import service
-
-class serviceAdmin(admin.ModelAdmin):
-    list_display=('service_token','service_link')
-
-admin.site.register(service,serviceAdmin)
-# Register your models here.
--- home3d/service/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-class service(models.Model):
-    service_token=models.CharField(max_length=50)
-    service_link=models.URLField(max_length=100)
-    
-# Create your models here.
